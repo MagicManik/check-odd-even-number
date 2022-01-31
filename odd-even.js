e738c54d13f95b9b3d989648585b67e9a18d1f08
MagicManik <manikislam1952@gmail.com>
2022-01-31 15:36:09 +0600

    check odd and even number

---
+++ odd-even.js
@@ -0,0 +1,58 @@
+// check even Number
+// my methord
+
+// function isEvenNumber(number) {
+//     var division = number % 2;
+//     if (division == 0) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+// var myNumber = 69;
+// var result = isEvenNumber(myNumber);
+// console.log(result);
+
+// var herNumber = 90;
+// var result = isEvenNumber(herNumber);
+// console.log(result);
+
+
+
+// methord of jhankar mahbum
+// check even number
+
+function isEven(number) {
+    if (number % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+var myNumber = 69;
+var result = isEven(myNumber);
+console.log(result);
+
+var herNumber = 90;
+var result = isEven(herNumber);
+console.log(result);
+
+
+// check odd number
+function isOdd(number) {
+    if (number % 2 == 1) {
+        return true;
+    }
+    else {
+        return false
+    }
+}
+var myNumber = 50;
+var result = isOdd(myNumber);
+console.log(result);
+
+var herNumber = 21;
+var result = isOdd(herNumber);
+console.log(result);
